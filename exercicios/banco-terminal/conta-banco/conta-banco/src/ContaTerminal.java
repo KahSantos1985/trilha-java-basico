9614581414c65de08e3eef2514f03ad9d2f1ef2f
Kah-PretaTech <katilene.santos32@gmail.com>
2024-07-02 00:19:44 -0300

    Desafio conta bancaria

--- exercicios/banco-terminal/conta-banco/conta-banco/src/ContaTerminal.java
+++ exercicios/banco-terminal/conta-banco/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite o número da conta");
+        int conta = scanner.nextInt();
+        
+        System.out.println("Digite o número da agência");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Digite o nome do cliente");
+        String nome = scanner.next();
+        
+        System.out.println("Digite o valor de depósito");
+        double saldo = scanner.nextDouble();
+
+    
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, " + nome + ", obrigado por criar uma conta em nosso banco. ");
+        System.out.println("sua agência é " + agencia + ", conta " + conta);
+        System.out.println("Seu saldo é de R$ " + saldo + " e já está disponível para saque.");
     }
 }
